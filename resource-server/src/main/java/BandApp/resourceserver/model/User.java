57bb0408b1eb7f6a00dd0fd9e470bf60dd8ffb35
Charlotte Kies <chopekies@gmail.com>
2023-01-29 11:24:58 -0500

    Configured a resource server to accept HTTP requests that come with a valid JWT token

---
+++ resource-server/src/main/java/BandApp/resourceserver/model/User.java
@@ -0,0 +1,22 @@
+package BandApp.resourceserver.model;
+
+public class User {
+
+    private final String id;
+
+    private final String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
