0c9b100a97e8c02d2bc3876a1a75f1384d6857eb
Charlotte Kies <chopekies@gmail.com>
2023-01-29 11:38:53 -0500

    Removed unused imports

--- resource-server/src/main/java/BandApp/resourceserver/configuration/SecurityConfig.java
+++ resource-server/src/main/java/BandApp/resourceserver/configuration/SecurityConfig.java
@@ -2,15 +2,11 @@ package BandApp.resourceserver.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
--- resource-server/src/main/java/BandApp/resourceserver/controller/UserController.java
+++ resource-server/src/main/java/BandApp/resourceserver/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
